87d788f15adbfae59955ced84bde544ffc5b0f22
Joris <joris.thiele@gmx.de>
2023-02-02 14:28:07 +0100

    initial commit
    Maps and Back4App set up

---
+++ app/src/main/java/de/joristhiele/findyourlacrosseevents/MainActivity.java
@@ -0,0 +1,17 @@
+package de.joristhiele.findyourlacrosseevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.ParseObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
